acc7c04bf0002e7d41bc5dcf05db6d889e43fe0c
sudeshnt <sudeshnt93@gmail.com>
2019-01-13 15:12:26 +0530

    product-list-item component

---
+++ app/modules/home/components/list-item/list-item.component.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, View } from "react-native";
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from "./list-item.styles";
+import PropTypes from 'prop-types';
+import {
+    CachedImage,
+    ImageCacheProvider
+} from 'react-native-cached-image';
+
+export default class ListItem extends Component {
+    render() {
+        const { item } = this.props.item;
+        return (
+            <TouchableOpacity style={styles.itemContainer}>
+                <ImageCacheProvider
+                    urlsToPreload={[item.image]}
+                    onPreloadComplete={() => console.log('hey there')}>
+                    <CachedImage style={styles.itemImage}
+                                 source={{ uri: item.image }}/>
+                </ImageCacheProvider>
+                <View style={styles.itemContent}>
+                    <View style={styles.itemDescription}>
+                        <Text>{item.description}</Text>
+                    </View>
+                    <View style={styles.itemDetails}>
+                        <View style={styles.container}>
+                            <TouchableOpacity>
+                                <Text style={{ color: '#2b9eb9' }}>{item.path}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.container}>
+                            <View style={{flexDirection: 'row' }}>
+                                <Icon style={styles.itemRatingIcon} name="ios-star" size={20}/>
+                                <Icon style={styles.itemRatingIcon} name="ios-star" size={20} />
+                                <Icon style={styles.itemRatingIcon} name="ios-star" size={20} />
+                                <Icon style={styles.itemRatingIcon} name="ios-star-outline" size={20} />
+                                <Icon style={styles.itemRatingIcon} name="ios-star-outline" size={20} />
+                            </View>
+
+                        </View>
+                    </View>
+                    <View style={styles.bottomRow}>
+                        <TouchableOpacity style={styles.bottomRowAction} numberOfLines={1}>
+                            <EvilIcon name="heart" size={25}/>
+                            <Text style={styles.bottomRowActionText}>Add to Wishlist</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.bottomRowAction} numberOfLines={1}>
+                            <EvilIcon name="cart" size={25}/>
+                            <Text style={styles.bottomRowActionText}>Add to Cart</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.itemPrice} numberOfLines={1}>$ 500.00</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+
+ListItem.propTypes = {
+    item: PropTypes.object.isRequired
+};
+
+ListItem.defaultProps = {
+    item: {}
+};
--- app/modules/home/screens/HomeScreen.js
+++ app/modules/home/screens/HomeScreen.js
@@ -3,12 +3,12 @@ import React from 'react';
 import {
   Text,
   View,
-  SafeAreaView,
+  FlatList
 } from 'react-native';
-import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import styles from './HomeScreen.styles';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ListItem from '../components/list-item/list-item.component'
 
 class HomeScreen extends React.Component<any, any> {
   static defaultProps: any
@@ -35,13 +35,40 @@ class HomeScreen extends React.Component<any, any> {
   }
 
   componentDidUpdate() {
-    
+
+  }
+
+  renderItem(item) {
+    return (
+        <ListItem item={item}/>
+    );
   }
 
   render() {
+    const itemList = [{
+        id: '1',
+        image: 'https://picsum.photos/200/300/?random',
+        description: 'Women\'s long wide sleeves square long dress specially for outdoor wear in multi colors',
+        path: 'Her, dresses',
+        subtitle: '',
+        price: '$500.00'
+    },
+    {
+        id: '1',
+        image: 'https://www.dike.lib.ia.us/images/sample-1.jpg/image',
+        description: 'Women\'s long wide sleeves square long dress specially for outdoor wear in multi colors',
+        path: 'Her, dresses',
+        subtitle: '',
+        price: '$500.00'
+    }];
     return (
       <View style={styles.container}>
         <Text>New home <Icon name="ios-book" color="#4F8EF7" /> screen!</Text>
+          <FlatList
+              data={itemList}
+              renderItem={(item) => this.renderItem(item)}
+              keyExtractor={(item, index) => index.toString()}
+          />
       </View>
     );
   }
@@ -57,7 +84,7 @@ HomeScreen.defaultProps = {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    
+
   };
 };
 
